566f2b6fa0c8ce6e659b566fb88391121d04d10a
amitgoel2k6 <49087454+amitgoel2k6@users.noreply.github.com>
2020-03-07 13:11:46 +0530

    Add files via upload

---
+++ Footer-tw-styles.js
@@ -0,0 +1,40 @@
+export default {
+    footerMainDiv: 'footerMainDiv mx-auto',
+    breadCrumbSection: 'breadCrumbSection bg-gray',
+    breadcrumbcontainer:'breadcrumbcontainer container py-20 md:flex md:justify-between mx-auto',
+    footerLinksSection: 'footerLinksSection flex flex-col-reverse md:flex md:flex-row justify-between',
+    legalText: 'legalText container mx-auto md:flex justify-between',
+    disclaimer: 'disclaimer mb-30',
+    copyright: 'copyright mb-40',
+    footermenu: 'footermenu mx-25 leading-xxloose text-16 md:w-8/12',
+    footerbreadcrumb: 'footerbreadcrumb text-14',
+    CountrySelectionFooter : 'CountrySelectionFooter hidden md:block',
+}
+
+export const linkStyles = {
+    default: {
+        container: 'container w-full',
+        title: 'title',
+        linkSection: 'linkSection flex flex-row my-20'
+    },
+    "related-brand-footer-styles": {
+        container: 'related-brand-footer-styles container mx-auto',
+        title: 'title text-darkblack text-30',
+        linkSection: 'linkSection flex flex-wrap justify-between md:w-full md:flex md:flex-row md:justify-start lg:flex lg:flex-row lg:justify-between'
+    }, 
+    "footerLinksSection": {
+        container: 'footerLinksSection container mx-auto',
+        
+    },
+    "SocialShare":  {
+        container: 'container mx-auto md:w-3/12',
+        title: 'title',
+        linkSection: 'SocialShare flex flex-row my-20  md:float-right'
+    },
+    "Footer-istudio-Business":  {
+        container: 'container mx-auto',
+        title: 'title',
+        linkSection: 'Footer-istudio-Business flex flex-row my-40'
+    },
+         
+}
